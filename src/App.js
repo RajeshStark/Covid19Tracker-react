efa6ebfc8be549e785c7dab84e82a10d713de43e
RajeshStark <sangapogurajesh@gmail.com>
2020-04-21 23:15:20 +0530

    version 1

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Cards, Charts, CountryPicker} from './components'
+import styles from './App.module.css'
+import { fetchData } from './api';
+
+class App extends React.Component{
+  state = {
+     date: {},
+     country: ''
+  }
+
+ async componentDidMount(){
+
+    const fetchedData = await fetchData()
+
+     this.setState({date: fetchedData})
+
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country)
+
+    this.setState({date: fetchedData, country: country})
+    
+    console.log(fetchedData);
+  }
+
+  render(){
+
+    return(
+      <div className={styles.container}>
+        <Cards data={this.state.date}/>
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Charts data={this.state.date} country={this.state.country} />
+      </div>
+    )
+  }
+}
+
+export default App;
