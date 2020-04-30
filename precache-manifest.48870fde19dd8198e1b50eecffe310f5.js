35f4eabf351b3860dd341a8afcd62abed6156ec7
RajeshStark <sangapogurajesh@gmail.com>
2020-04-30 13:10:20 +0530

    Updates

---
+++ precache-manifest.48870fde19dd8198e1b50eecffe310f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61414d1c6da0457e98540d9b58bb796b",
+    "url": "/Covid19Tracker-react/index.html"
+  },
+  {
+    "revision": "d72d2cd821a43bac2010",
+    "url": "/Covid19Tracker-react/static/css/main.6334632f.chunk.css"
+  },
+  {
+    "revision": "cb3a1225707e7f1cfdb0",
+    "url": "/Covid19Tracker-react/static/js/2.a39b8c69.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker-react/static/js/2.a39b8c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72d2cd821a43bac2010",
+    "url": "/Covid19Tracker-react/static/js/main.5f4aabc9.chunk.js"
+  },
+  {
+    "revision": "49b711d39544605c45aa",
+    "url": "/Covid19Tracker-react/static/js/runtime-main.4ac034b1.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Cards } from './Cards/Cards';
-export { default as Charts } from './Chart/Charts';
-export { default as CountryPicker } from './CountryPicker/CountryPicker'
